54f91516d6b3d493e04e63f9b9e0212d81efbf13
Kdoni <63546827+Kdoni@users.noreply.github.com>
2021-07-14 14:31:30 +0100

    initial commit

---
+++ app/src/main/java/com/example/fragmentmanager/fragments/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.example.fragmentmanager.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
+import com.example.fragmentmanager.R
+import com.example.fragmentmanager.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+private lateinit var  binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding=FragmentHomeBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+}
