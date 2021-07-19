c0eb4016e5745827be117a3954dd5994789966df
Kdoni <63546827+Kdoni@users.noreply.github.com>
2021-07-19 13:46:49 +0100

    initial commit

--- app/src/main/java/com/example/fragmentmanager/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/fragmentmanager/fragments/HomeFragment.kt
@@ -1,23 +1,45 @@
 package com.example.fragmentmanager.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.*
 import com.example.fragmentmanager.R
 import com.example.fragmentmanager.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
-private lateinit var  binding: FragmentHomeBinding
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var manager: FragmentManager
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding=FragmentHomeBinding.inflate(inflater, container, false)
-        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        manager = requireActivity().supportFragmentManager
+        binding.apply {
+            nextButton.setOnClickListener {
+                openNextFragmentKt()
+            }
+        }
         return binding.root
+    }
+
+    private fun openNextFragmentKt() {
+        manager.commit {
+            replace<NewFragment>(R.id.fragmentContainerView)
+            setReorderingAllowed(true)
+            addToBackStack("")
+        }
+
+    }
+
+    private fun openNextFragmentJv() {
+        manager.beginTransaction()
+            .replace(R.id.fragmentContainerView, NewFragment::class.java, null)
+            .setReorderingAllowed(true)
+            .addToBackStack(null)
+            .commit()
     }
 }
